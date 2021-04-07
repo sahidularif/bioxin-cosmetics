9543cbc52dad9b5a19b67d91bd9cf5f6094569a8
sahidularif <sahidularif1@gmail.com>
2021-04-07 06:55:06 +0600

    Initial commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZi4Y81x0Ytgtl_MxJW2w_2-RQwYPVA00",
+  authDomain: "auth-recap-299a4.firebaseapp.com",
+  projectId: "auth-recap-299a4",
+  storageBucket: "auth-recap-299a4.appspot.com",
+  messagingSenderId: "729510149068",
+  appId: "1:729510149068:web:04142a9d180bafd0ed7b69"
+  };
+  export default firebaseConfig;
